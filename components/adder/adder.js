e850871fecdfbf5b4bc83ac226fc9bf0f88c32c7
Azhar Alali <46798680+AzharAlali@users.noreply.github.com>
2019-10-11 12:29:10 -0500

    adding files

---
+++ components/adder/adder.js
@@ -0,0 +1,17 @@
+const divide = (x, y) => { return x / y }
+
+const adder = new Vue({
+  el: '#adder',
+  data: {
+    guest: 'Azhar',
+    firstNumber: 10,
+    secondNumber: 2
+  },
+  computed: {
+    result: function () {
+      const i = parseInt(this.firstNumber)
+      const j = parseInt(this.secondNumber)
+      return `${this.guest}, your sum is ${divide(i, j)}.`
+    }
+  }
+})
